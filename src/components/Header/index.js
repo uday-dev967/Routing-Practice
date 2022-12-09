84ce67004d5e0c1fb057c6a16c4a1f3df5fdac83
udaykanda967 <udaychakravarthi22@gmail.com>
2022-12-09 15:25:17 +0000

    adds files

---
+++ src/components/Header/index.js
@@ -0,0 +1,36 @@
+import {Link} from 'react-router-dom'
+import './index.css'
+
+const Header = () => (
+  <nav className="nav-header">
+    <div className="blog-container">
+      <div className="title-logo">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/wave-logo-img.png"
+          alt="wave"
+          className="logo"
+        />
+        <h1 className="blog-title">wave</h1>
+      </div>
+      <ul className="nav-menu">
+        <li>
+          <Link className="nav-link" to="/">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link className="nav-link" to="/about">
+            About
+          </Link>
+        </li>
+        <li>
+          <Link className="nav-link" to="contact">
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+)
+
+export default Header
